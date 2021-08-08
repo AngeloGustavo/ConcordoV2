3b9282d033210a112e6e36d8b3ebc1286a11c61e
AngeloGustavo <angelogustavo157@gmail.com>
2021-08-07 22:27:05 -0300

    Criação da pasta canais_servidores

--- src/concordo.cpp
+++ src/concordo.cpp
@@ -5,12 +5,20 @@
 #include "sistema.h"
 #include "executor.h"
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 using namespace std;
 
 int main() {
   // inicialização do sistema
   Sistema sistema;
   Executor executor(sistema);
+
+  mkdir("../data/canais_servidores",0777);
   sistema.load_user();
   sistema.load_server();
   // o executor recebe o cin e o cout. Ele os utilizará para ler as linhas
